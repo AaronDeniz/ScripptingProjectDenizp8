e65e50e47c14fd0f9cfc91e125125f038d016043
Aaron  Deniz <145504641+AaronDeniz@users.noreply.github.com>
2023-11-09 14:49:08 -0800

    did Loop
    did Loop

---
+++ ScripptingProjectDeniz08/Assets/Scrptis/Variablecript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variablecript : MonoBehaviour
+{
+   public int myint = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myint = MultiplyByTwo(myint);
+        Debug.Log(myint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
